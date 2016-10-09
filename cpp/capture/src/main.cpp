f0eec33afa0d90e8171ddf2932b5531547120734
Sergei Nikulov <sergey.nikulov@gmail.com>
2016-10-09 17:35:34 +0300

    added fallback to mjpeg if no h.264 stream found

--- cpp/capture/src/main.cpp
+++ cpp/capture/src/main.cpp
@@ -8,15 +8,20 @@ int main(int, char**)
     cv::VideoCapture vcap;
     cv::Mat image;
 
-    const std::string videoStreamAddress = "rtsp://admin:admin@212.45.31.140:554/h264";
+    const std::string vsH264 = "rtsp://admin:admin@212.45.31.140:554/h264";
+    const std::string vsMJPEG = "rtsp://admin:admin@212.45.31.140:554/jpeg";
     /* it may be an address of an mjpeg stream, 
     e.g. "http://user:pass@cam_address:8081/cgi/mjpg/mjpg.cgi?.mjpg" */
 
     //open the video stream and make sure it's opened
-    if (!vcap.open(videoStreamAddress))
+    if (!vcap.open(vsH264))
     {
-        std::cout << "Error opening video stream or file" << std::endl;
-        return -1;
+        std::cout << "Error opening video stream H.264, Trying MJPEG..." << std::endl;
+        if (!vcap.open(vsMJPEG))
+        {
+            std::cout << "Error opening video stream MJPEG... Exiting..." << std::endl;
+            return -1;
+        }
     }
 
     //Create output window for displaying frames.
