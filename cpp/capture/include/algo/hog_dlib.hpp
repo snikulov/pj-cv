b1209543d6e46ffce953ee30b28a55222742907c
Sergei Nikulov <sergey.nikulov@gmail.com>
2016-10-30 20:10:04 +0300

    HOG: prelimenarly drop

--- cpp/capture/include/algo/hog_dlib.hpp
+++ cpp/capture/include/algo/hog_dlib.hpp
@@ -4,13 +4,39 @@
 
 #include "algo/od_interface.hpp"
 
+#include <dlib/opencv/cv_image.h> 
+#include <dlib/svm_threaded.h>
+#include <dlib/image_processing.h>
+#include <dlib/data_io.h>
+#include <dlib/gui_widgets.h>
+#include <date.h>
+
+using img_scanner_t = dlib::scan_fhog_pyramid<dlib::pyramid_down<6>>;
+
 class hog : public od_interface
 {
 public:
-     bool has_objects(data_t& d)
-     {
-     }
-     
+    explicit hog(const std::string& path_to_svm)
+        : detector_(new dlib::object_detector<img_scanner_t>())
+        , svm_path_(path_to_svm)
+    {
+        using namespace date;
+        std::cout << std::chrono::system_clock::now() << " loading SVM from " << svm_path_ << std::endl;
+        dlib::deserialize(svm_path_) >> *detector_;
+        std::cout << std::chrono::system_clock::now() << " finished loading" << std::endl;
+    }
+
+    bool has_objects(data_t& d)
+    {
+        cv::Mat tmp = *d.frame_;
+        dlib::cv_image<dlib::bgr_pixel> cimg(tmp);
+        std::vector<dlib::rectangle> dets = (*detector_)(cimg);
+        return !dets.empty();
+    }
+
+private:
+    std::unique_ptr<dlib::object_detector<img_scanner_t> > detector_;
+    std::string svm_path_;
 };
 
 #endif  // HOG_DLIB_HPP__
--- cpp/capture/src/main.cpp
+++ cpp/capture/src/main.cpp
@@ -15,6 +15,7 @@
 
 #include "algo/od_interface.hpp"
 #include "algo/hough_circles.hpp"
+#include "algo/hog_dlib.hpp"
 
 namespace app = boost::application;
 namespace po  = boost::program_options;
@@ -26,12 +27,13 @@ class capture : private boost::noncopyable
 public:
     capture(
         app::context& context, const std::string& url, monitor_queue<data_t>& q, const fs::path& p,
-        const std::string& algo)
+        const std::string& algo, const std::string& svm_path)
         : context_(context)
         , url_(url)
         , queue_(q)
         , img_path_(p)
         , algo_(algo)
+        , svm_path_(svm_path)
     {
     }
 
@@ -56,7 +58,16 @@ public:
         }
         else if (algo_ == "hog")
         {
-
+            fs::path p{svm_path_};
+            if (fs::exists(p) && fs::is_regular_file(p))
+            {
+                detector.reset(new hog(svm_path_));
+            }
+            else
+            {
+                std::cout << "File not found: " << fs::absolute(p).string() << std::endl;
+                return -1;
+            }
         }
         else
         {
@@ -115,6 +126,7 @@ private:
     monitor_queue<data_t>& queue_;
     fs::path img_path_;
     std::string algo_;
+    std::string svm_path_;
 };
 
 int main(int argc, char* argv[])
@@ -127,9 +139,11 @@ int main(int argc, char* argv[])
     // clang-format off
     desc.add_options()
         ("help,h", "Help screen")
-        ("url,u", po::value<std::string>(&url)->default_value("rtsp://admin:admin@212.45.31.140:554/h264"),"camera url")
+        ("url,u", po::value<std::string>(&url)->default_value("rtsp://admin:admin@212.45.31.140:554/h264"), "camera url")
         ("out,o", po::value<std::string>(&out_dir)->default_value("images"), "output folder")
-        ("algo, a", po::value<std::string>(&out_dir)->default_value("circles"), "detection algorithm[circles|hog]");
+        ("algo, a", po::value<std::string>(&out_dir)->default_value("circles"), "detection algorithm[circles|hog]")
+        ("svmpath, s", po::value<std::string>(&out_dir)->default_value("object_detector.svm"), "path to obj detector svm")
+        ;
     // clang-format on
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -145,11 +159,12 @@ int main(int argc, char* argv[])
         }
 
         algo = vm["algo"].as<std::string>();
+        auto svm_path = vm["svmpath"].as<std::string>();
 
         app::context app_context;
         monitor_queue<data_t> frame_q;
 
-        capture app(app_context, url, frame_q, outp, algo);
+        capture app(app_context, url, frame_q, outp, algo, svm_path);
 
         app_context.insert<app::args>(app::csbl::make_shared<app::args>(argc, argv));
 
